2bdb62841da3474cfc35b5f0643930d182e6f86b
lurosas89 <lurosas@itba.edu.ar>
2023-10-08 18:21:04 -0300

    Exp05_GrupoA

---
+++ workflow-inicial/lag2_delta_off/601_RUN_correr_workflow.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/dm2023b/src/workflow-inicial/lag2_delta_off/611_CA_reparar_dataset.r")
+source("~/dm2023b/src/workflow-inicial/lag2_delta_off/621_DR_corregir_drifting.r")
+source("~/dm2023b/src/workflow-inicial/lag2_delta_off/631_FE_historia.r")
+source("~/dm2023b/src/workflow-inicial/lag2_delta_off/641_TS_training_strategy.r")
+
+# ultimos pasos, muy lentos
+source("~/dm2023b/src/workflow-inicial/lag2_delta_off/651_HT_lightgbm.r")
+source("~/dm2023b/src/workflow-inicial/lag2_delta_off/661_ZZ_final.r")
